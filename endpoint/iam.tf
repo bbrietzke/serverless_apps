b3ba4fe68b35d3cffb3979caba4c4da3d0295d24
Brian Brietzke <bbrietzke+github@gmail.com>
2022-01-15 22:06:10 -0600

    terraform fmt

--- endpoint/iam.tf
+++ endpoint/iam.tf
@@ -1,8 +1,8 @@
 
-# resource "aws_iam_role_policy_attachment" "lambda_policy" {
-#   role       = aws_iam_role.lambda_exec.name
-#   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-# }
+resource "aws_iam_role_policy_attachment" "lambda_policy" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
 
 resource "aws_iam_role" "lambda_exec" {
   name = format("%s-%s", var.application_name, var.resource)
